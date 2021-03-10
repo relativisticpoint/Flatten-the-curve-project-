cdebfac7cbdb331739b26b568696f7427226874f
QVPham9601 <vietpham961@gmail.com>
2021-03-10 22:00:16 +0100

    Person class

--- Person.java
+++ Person.java
@@ -10,28 +10,30 @@ public class Person {
 	
 	//Parameters
 	
-	
-	public Vec velocity;
+	public static final double RADIUS = 10.0;
 	public Vec position;
+	public Vec velocity;
 	public Color status = Color.green;
 	public boolean wearMask = false;
 
 	
 	// Constructor
-	public Person (Vec Position, Vec Velocity) {
-		position = Position;
-		velocity = Velocity;
+	public Person (Vec initPosition, Vec initVelocity) {
+		position = initPosition;
+		velocity = initVelocity;
 	}
 		
 	// To change the status of a person
 	public void changeStatus (Color newStatus) {
 		status = newStatus;
 	}
-	//to move 
-	public void movement(){
+	
+	// To move a person
+	public void movement() {
+		while (position.x + velocity.x <0 || position.x + velocity.x >1500 || position.y + velocity.y <0 || position.y + velocity.y >1500) {
+			velocity = new Vec (20.0*Math.random()-10.0, 20.0*Math.random()-10.0);
+		}
 		position.add(velocity);
 	}
-	
-
 }
 
