35379d2ed13b0e5b674279feb41d8b9232db69d7
QVPham9601 <vietpham961@gmail.com>
2021-03-11 21:35:37 +0100

    Population class

--- Population.java
+++ Population.java
@@ -19,15 +19,27 @@ public class Population {
 	public Population(int initNb) {
 		nbOfPeople = initNb;
 		everyone = new ArrayList<Person>();
-		while (everyone.size() < nbOfPeople) {
-			Person newPerson = new Person (new Vec(1150*Math.random(),1150*Math.random()),				//If the simulation is 1200x1200 pixels
-			                               new Vec(20.0*Math.random()-10.0,20.0*Math.random()-10.0));   //The velocity is in [-10,10]
+		
+		//To make "everyone" not empty
+		Person newPerson = new Person (new Vec(1150*Math.random(),1150*Math.random()), new Vec(20.0*Math.random()-10.0,20.0*Math.random()-10.0)); 
+		everyone.add (newPerson);
+		
+		//To generate a population with the desired number of people
+		while (everyone.size() < nbOfPeople) {			
+			
+			newPerson = new Person (new Vec(1150*Math.random(),1150*Math.random()),				 //If the simulation is 1200x1200 pixels
+			                        new Vec(20.0*Math.random()-10.0,20.0*Math.random()-10.0));   //The velocity is in [-10,10]
+			
+			while (this.coincide(newPerson)) {
+				newPerson = new Person (new Vec(1150*Math.random(),1150*Math.random()),new Vec(20.0*Math.random()-10.0,20.0*Math.random()-10.0));
+			}			
 			everyone.add (newPerson);
 		}
 	}
 	
 	//To create "the moving population"
 	public void newWorld() {
+		
 		if (!everyone.isEmpty()){
 			for (Person a : everyone) {
 				a.movement();
@@ -36,17 +48,28 @@ public class Population {
 	}
 	
 	//To verify all people do not "step onto each other"
-	public boolean notCoincide(Person p) {
+	public boolean coincide(Person p) {
 		if (!everyone.isEmpty()){
 			for (Person b : everyone) {
 				if (b.different(p)) {
 					if (b.position.dist(p.position) <= 2*b.RADIUS) {
-						return false;
+						return true;
 					}
 				}
 			}
 		}
-		return true;
+		return false;
+	}
+	
+	public boolean movingPossible (Person p) {
+		if (!everyone.isEmpty()) {
+			if (p.outWindow()) {
+				return false;
+			}
+		}
+		
+		Person p1 = new Person (new Vec(p.position.x + p.velocity.x, p.position.y + p.velocity.y), new Vec (p.velocity.x,p.velocity.y));
+		return !this.coincide(p1);
 	}
 }
 	
