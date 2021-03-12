64dd8473f3f9e30264948fb3bfd5168c290d64c3
QVPham9601 <vietpham961@gmail.com>
2021-03-13 00:44:25 +0100

    Plot the Faces

--- PlotTheFaces.java
+++ PlotTheFaces.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 
 
 public class PlotTheFaces extends JFrame implements ActionListener {
+	
 	//attributes
 	private int time =0;
 	private Timer monChrono;
@@ -13,49 +14,51 @@ public class PlotTheFaces extends JFrame implements ActionListener {
 	private int countToChangeVelocity =0;
 	
 	//constructor
-	public PlotTheFaces(){
-		faces = new Population(10);
+	public PlotTheFaces(int initNbOfPeople){
+		faces = new Population(initNbOfPeople);
 		this.setTitle ("Flatten the Curve");
 		this.setLayout (null);
-		this.setSize (800,800);
+		this.setSize (1200,800);
 		this.setLocation (50,50);
 		this.setResizable(true);
 		this.setVisible (true);
+		
 		monChrono = new Timer (100,this);
 		monChrono.start();
 	}
-	/*public void addFaces (Person p){
-		faces.add(p);
-		repaint(); //update the visualization after modifying
-	}*/
+
 	public void paint(Graphics g){
 		g.setColor(Color.blue);
 		g.fillRect(0,0,this.getWidth(),this.getHeight());
 		if (!faces.everyone.isEmpty()){
 			for (Person p : faces.everyone){
-				p.drawFaces(g);  //draw all the faces
+				
+				//draw all the faces
+				p.drawFaces(g);  							
 			}
 		}
 	}
+	
 	public void actionPerformed (ActionEvent e){
 		if (e.getSource()==monChrono){
 			time = time + 100;
 			this.setTitle ("Flatten the curve - Playing time: "+time/100+"s");
-			faces.newWorld();
+			faces.updateWorld();
 			repaint();
 			countToChangeVelocity++;
 			
-			if (countToChangeVelocity==100) {
-				faces.changeVelocity();
+			if (countToChangeVelocity==20) {
+				for (Person a : faces.everyone) {
+					if (Math.random() <0.5) {
+						a.changeVelocity();
+					}
+				}
 				countToChangeVelocity =0;
 			}
 			
 		}
 	}
 
-	public void lancement () {
-		monChrono.start();
-		
-	}
+	
 	 
 }
