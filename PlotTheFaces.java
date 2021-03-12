1e00d5a08c79858082cce142b4b9fa70963f8b77
QVPham9601 <vietpham961@gmail.com>
2021-03-12 23:23:52 +0100

    Plot the Faces

--- PlotTheFaces.java
+++ PlotTheFaces.java
@@ -1,52 +1,55 @@
-/* Moving objects - Faces
- */
 import javax.swing.*;
 import java.util.ArrayList; 
 import java.awt.Color;
 import java.awt.event.*;
 import java.awt.Graphics;
 
-	
+
 public class PlotTheFaces extends JFrame implements ActionListener {
 	//attributes
 	private int time =0;
 	private Timer monChrono;
-	private ArrayList <Person> faces;
+	private Population faces;
+	private int countToChangeVelocity =0;
+	
 	//constructor
 	public PlotTheFaces(){
-		faces = new ArrayList <Person>();
+		faces = new Population(10);
 		this.setTitle ("Flatten the Curve");
 		this.setLayout (null);
-		this.setSize (600,600);
-		this.setLocation (700,200);
+		this.setSize (800,800);
+		this.setLocation (50,50);
 		this.setResizable(true);
 		this.setVisible (true);
-		monChrono = new Timer (500,this);
+		monChrono = new Timer (100,this);
+		monChrono.start();
 	}
-	public void addFaces (Person p){
+	/*public void addFaces (Person p){
 		faces.add(p);
 		repaint(); //update the visualization after modifying
-	}
+	}*/
 	public void paint(Graphics g){
-		g.setColor(Color.orange);
+		g.setColor(Color.blue);
 		g.fillRect(0,0,this.getWidth(),this.getHeight());
-		boolean emptyCheck = faces.isEmpty();
-		if (emptyCheck==false){
-			for (Person p : faces){
+		if (!faces.everyone.isEmpty()){
+			for (Person p : faces.everyone){
 				p.drawFaces(g);  //draw all the faces
 			}
 		}
 	}
 	public void actionPerformed (ActionEvent e){
 		if (e.getSource()==monChrono){
-			time = time + 1000;
-			this.setTitle ("Flatten the curve - Playing time: "+time/1000+"s");
-			if (faces!=null) {
-				for (Person p : faces){
-					p.movement();
-				}
+			time = time + 100;
+			this.setTitle ("Flatten the curve - Playing time: "+time/100+"s");
+			faces.newWorld();
 			repaint();
+			countToChangeVelocity++;
+			
+			if (countToChangeVelocity==100) {
+				faces.changeVelocity();
+				countToChangeVelocity =0;
 			}
+			
 		}
 	}
 
@@ -56,7 +59,3 @@ public class PlotTheFaces extends JFrame implements ActionListener {
 	}
 	 
 }
-	
-	
-
-
