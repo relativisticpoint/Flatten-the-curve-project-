90527960335c463d42c7587f21fd19824b520aef
QVPham9601 <vietpham961@gmail.com>
2021-03-19 16:18:13 +0100

    DeadFace - Child class Ver 2

--- DeadFace.java
+++ DeadFace.java
@@ -13,24 +13,30 @@ public class DeadFace extends Person {
 	public DeadFace (){
 		super();
 	}
+	
 	public DeadFace (Vec initPosition, Vec initVelocity){
 		super(initPosition, initVelocity);
 	}
+	
 	public Person changeStatus (){
 		Vec newPosition = this.position;
 		Vec newVelocity = new Vec(0.0,0.0);
 		DeadFace newDeath = new DeadFace (newPosition,newVelocity);
 		return newDeath;
 	}
-	public Person hasRecovered (){
-		Vec newPos = this.position;
-		Vec newVelo = this.velocity;
-		SmileyFace newRecovered = new SmileyFace (newPos,newVelo);
-		return newRecovered;
+	
+	public Vec setNewRandomVelocity() {
+		return new Vec(0.0,0.0);
 	}
+	
 	public void drawFaces (Graphics g){
 		g.setColor(status);
 		g.fillOval ((int)(position.x), (int)(position.y),(int)(2*RADIUS), (int)(2*RADIUS));
+	}
+	
+	public Person hasRecovered (){
+		DeadFace newRecovered = null;
+		return newRecovered;
 	}
 }
 
