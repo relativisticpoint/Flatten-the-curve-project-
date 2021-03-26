b9219169e0be5dbd776996bbd771f482c17bac73
QVPham9601 <vietpham961@gmail.com>
2021-03-26 16:28:22 +0100

    Ill Faces

--- IllFace.java
+++ IllFace.java
@@ -8,33 +8,42 @@ import javax.swing.*;
 public class IllFace extends Person { 
 	//attributes
 	private Color status = Color.red;
+	
 	private int INFECT_RADIUS = 20;
+	
 	//constructor
 	public IllFace (){
 		super();
 	}
-	public IllFace (Vec initPosition, Vec initVelocity){
-		super(initPosition, initVelocity);
+	
+	public IllFace (Vec initPosition, Vec initVelocity, int nb){
+		super(initPosition, initVelocity, nb);
 	}
+	
 	//when a patient dies
 	public Person changeStatus (){
 		Vec newPosition = this.position;
 		Vec nilVelo = new Vec (0.0,0.0);
-		DeadFace newDeath = new DeadFace (newPosition,nilVelo);
+		int newFamilyNb = this.familyNb;
+		DeadFace newDeath = new DeadFace (newPosition,nilVelo,newFamilyNb);
 		return newDeath;
 	}
+	
 	//when a patient has recovered
 	public Person hasRecovered (){
 		Vec newPos = this.position;
 		Vec newVelo = this.velocity;
-		SmileyFace newRecovered = new SmileyFace (newPos,newVelo);
+		int newFamilyNb = this.familyNb;
+		SmileyFace newRecovered = new SmileyFace (newPos,newVelo, newFamilyNb);
 		return newRecovered;
 	}
+	
+	
 	//representation	
 	public void drawFaces (Graphics g){
 		g.setColor(status);
-		g.fillOval ((int)(position.x), (int)(position.y),(int)(2*RADIUS), (int)(2*RADIUS));
-		g.drawOval ((int)(position.x-INFECT_RADIUS), (int)(position.y-INFECT_RADIUS),(int)(2*(RADIUS+INFECT_RADIUS)), (int)(2*(RADIUS+INFECT_RADIUS)));
+		super.drawFaces(g);
+		//g.drawOval ((int)(position.x-INFECT_RADIUS), (int)(position.y-INFECT_RADIUS),(int)(2*(RADIUS+INFECT_RADIUS)), (int)(2*(RADIUS+INFECT_RADIUS)));
 	}
 }
 
