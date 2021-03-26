d47580720c3a5969917464ccde3d0a7ab60e98c0
QVPham9601 <vietpham961@gmail.com>
2021-03-26 16:26:43 +0100

    Population class

--- Population.java
+++ Population.java
@@ -22,20 +22,29 @@ public class Population {
 		nbOfPeople = initNb;
 		everyone = new ArrayList<Person>();
 		
-		//To make "everyone" not empty
 		Person newPerson = new SmileyFace ();
-		newPerson = new SmileyFace (newPerson.setNewRandomPosition(), newPerson.setNewRandomVelocity()); 
-		everyone.add (newPerson);
+		
+		int[] familyMembersNb = this.generateNbOfMembers();
+		int count =0;
+		int family =1;
 		
 		//To generate a population with the desired number of people
 		while (everyone.size() < nbOfPeople) {						
-			newPerson = new SmileyFace (newPerson.setNewRandomPosition(),newPerson.setNewRandomVelocity()); 
+			newPerson = new SmileyFace (newPerson.setNewRandomPosition(),newPerson.setNewRandomVelocity(), family); 
 			  					
 			while (this.coincideWhenInitiate(newPerson)) {
-				newPerson = new SmileyFace(newPerson.setNewRandomPosition(), newPerson.setNewRandomVelocity());
+				newPerson = new SmileyFace(newPerson.setNewRandomPosition(), newPerson.setNewRandomVelocity(), family);
 			}			
 			everyone.add (newPerson);
+			
+			count++;
+			
+			if (count >= familyMembersNb[family-1]) {
+				family ++;
+				count = 0;
+			}
 		}
+		
 	}
 	
 	//To verify all people are not "spawn on top of each other"
@@ -52,38 +61,67 @@ public class Population {
 		return false;
 	}
 	
+	//Method to distribute randomly people to a family consisting of from 2 to 6 people
+	public int[] generateNbOfMembers() {
+		int[] familyDistribution = new int[20];
+		int sum = 0;
+		while (sum >70 || sum <50) {
+			for (int i =0; i<15; i++) {
+				familyDistribution[i] = (int)(5*Math.random())+2;
+				sum += familyDistribution[i];
+			}
+		}
+
+		int difference = 80 -sum;
+		int remainder = difference %5;
+		for (int i =15; i<15 + remainder; i++) {
+			familyDistribution[i] = (int)(difference/5)+1;
+		}
+		for (int i =15+remainder; i<20; i++) {
+			familyDistribution[i] = (int)(difference/5)+1;
+		}
+		return familyDistribution;
+	}
+	
+	
 	//To change everyone's velocity after some time
 	public void newVelocity () {
-		for (Person a : everyone) {	
-			if (!(a instanceof DeadFace)){		 
-				if (Math.random() <0.5) {
-					a.velocity =  a.setNewRandomVelocity();
-				}
+		for (Person a : everyone) {			 
+			if (Math.random() <0.5) {
+				a.velocity =  a.setNewRandomVelocity();
 			}		
 		}
 	}
 	
 	//To update the world in general
 	public void updateWorld() {
-		this.updateWorldInfection(); //error
+		this.updateWorldInfection(); 
 		this.updateWorldMovement();
+		
+	}
+	
+	public void updateWorldLockdown() {
+		this.updateWorldInfection(); 
+		this.updateWorldMovementLockdown();
 		
 	}
 	
 	//To start the infection
 	public void startTheInfection(double x) {
 		if (x == ONE_DAY) {
-			for (int i =0; i< (int)(5.0*Math.random()+1.0); i++) {
-				everyone.add(everyone.get(i).changeStatus());
-				everyone.remove(i);
+			for (int i =0; i< (int)(4*Math.random()+2); i++) {
+				int nb = (int)(80.0*Math.random());
+				everyone.add(everyone.get(nb).changeStatus());
+				everyone.remove(nb);
 			}
 		}
 	}
 	
 	//To update the world with disease	
 	public void updateWorldInfection () {
-		for(int i = 0; i <everyone.size(); i++) { //error
+		for(int i = 0; i <everyone.size(); i++) { 
 			Person p = everyone.get(i);
+			
 			//A healthy person may get infected after 3 days
 			if (p.timeToBeInfected >0) {
 				if (p.timeToBeInfected == 3*ONE_DAY) {
@@ -109,7 +147,7 @@ public class Population {
 				}else{
 					p.infectionTime += 100.0;
 					if (p.infectionTime == 10*ONE_DAY) {
-						everyone.add(p.changeStatus());
+						everyone.add(0,p.changeStatus());
 						everyone.remove(p);
 						p.infectionTime = 0.0;
 					}
@@ -131,11 +169,34 @@ public class Population {
 	
 	//To create "the moving population"
 	public void updateWorldMovement () {
+		int i =0;
+		if (!everyone.isEmpty()){					
+			for (Person a : everyone) {	
+				while (this.movingImpossible(a)) {	
+
+				
+					a.velocity = a.setNewRandomVelocity();
+					i++;
+				}
+					
+				
+				a.movement();
+			}
+		}
+	}
+	
+	public void updateWorldMovementLockdown () {
+		int i =0;
 		if (!everyone.isEmpty()){					
 			for (Person a : everyone) {
-						
-				while (this.movingImpossible(a)) {	
+				if (a.distanceFromHome() >a.HOUSE_RADIUS) { 
+					a.velocity = a.goHome();	
+				}
+					
+				while (this.movingImpossible(a) || a.goAwayFromHome()) {	
+					
 					a.velocity = a.setNewRandomVelocity();
+					i++;
 				}
 				
 				a.movement();
@@ -149,8 +210,8 @@ public class Population {
 		int count =0;
 		if (!everyone.isEmpty()){
 			for (Person b : everyone) {
-				if (b.different(p) ) {
-					if (b.position.dist(p.position) <= 2*b.RADIUS) {
+				if (b.different(p) && !(b instanceof DeadFace)) {
+					if (b.position.dist(p.position) < 2*b.RADIUS) {
 						count++;
 						if (count==2) {
 							return true;
@@ -163,8 +224,10 @@ public class Population {
 	}
 	
 	public boolean movingImpossible (Person p) {
-		
-		Person p1 = new SmileyFace (new Vec((p.position).x + (p.velocity).x, (p.position).y + (p.velocity).y), new Vec ((p.velocity).x,(p.velocity).y));
+		if (p instanceof DeadFace) {
+			return false;
+		}
+		Person p1 = new SmileyFace (new Vec((p.position).x + (p.velocity).x, (p.position).y + (p.velocity).y), new Vec ((p.velocity).x,(p.velocity).y),1);
 		return (p.outWindow() || this.coincideAfterMoving(p1));
 	}
 	
